ea265e2970439d968f096c487c252bff11337f0e
Bilbo L.R <bilbo.lr@gmail.com>
2024-04-19 10:58:04 +0000

    chronooo

--- src/js/component/SimpleCounter.jsx
+++ src/js/component/SimpleCounter.jsx
@@ -6,21 +6,46 @@ const SimpleCounter = (props) => {
     return (
         <div className='chrono'>
             <div className='clockIcone'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
                     <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
                     <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
                 </svg>
             </div>
-            <div className='sixthNumber'>0</div>
-            <div className='fifthNumber'>0</div>
-            <div className='forthNumber'>0</div>
-            <div className='thirdNumber'>0</div>
-            <div className='secondNumber'>0</div>
-            <div className='fristNumber'>0</div>
+            <div className='eighthNumber'>{props.eighthDigit % 10}</div>
+            <div className='seventhNumber'>{props.seventhDigit % 10}</div>
+            <div className='sixthNumber'>{props.sixthDigit % 10}</div>
+            <div className='fifthNumber'>{props.fifthDigit % 10}</div>
+            <div className='forthNumber'>{props.forthDigit % 10}</div>
+            <div className='thirdNumber'>{props.thirdDigit % 10}</div>
+            <div className='point'>,</div>
+            <div className='secondNumber'>{props.secondDigit % 10}</div>
+            <div className='firstNumber'>{props.firstDigit % 10}</div>
         </div>
     )
 };
 
+let counter = 0;
+
 setInterval(function(){
-    ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+    const eighthNumber = Math.floor(counter/10000000);
+    const seventhNumber = Math.floor(counter/1000000);
+    const sixthNumber = Math.floor(counter/100000);
+    const fifthNumber = Math.floor(counter/10000);
+    const forthNumber = Math.floor(counter/1000);
+    const thirdNumber = Math.floor(counter/100);
+    const secondNumber = Math.floor(counter/10);
+    const firstNumber = Math.floor(counter/1);
+    counter ++;
+        ReactDOM.render(<SimpleCounter 
+            firstDigit = {firstNumber} 
+            secondDigit = {secondNumber} 
+            thirdDigit = {thirdNumber}
+            forthDigit = {forthNumber}
+            fifthDigit = {fifthNumber}
+            sixthDigit = {sixthNumber}
+            seventhDigit = {seventhNumber}
+            eighthDigit = {eighthNumber} 
+            />, document.querySelector("#app"));
 }, 500);
+
+export default SimpleCounter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import SimpleCounter from "./SimpleCounter.jsx";
+import SimpleCounter from "./SimpleCounter.jsx"; 
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
 	return (
 		<SimpleCounter />
