270fbf41cccc8ece8fabaffff23472b540a4205b
Bilbo L.R <bilbo.lr@gmail.com>
2024-04-22 11:16:50 +0000

    simplecount

--- src/js/component/SimpleCounter.jsx
+++ src/js/component/SimpleCounter.jsx
@@ -1,51 +1,53 @@
-import React from "react";
-import PropTypes from 'prop-types';
 
+import React, { useState, useEffect } from "react";
 
-const SimpleCounter = (props) => {
+const SimpleCounter = () => {
+    const [counter, setCounter] = useState(0);
+    
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCounter(prevCounter => prevCounter + 1);
+        }, 100);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    const eighthNumber = Math.floor(counter / 10000000) % 10;
+    const seventhNumber = Math.floor(counter / 1000000) % 10;
+    const sixthNumber = Math.floor(counter / 100000) % 10;
+    const fifthNumber = Math.floor(counter / 10000) % 10;
+    const forthNumber = Math.floor(counter / 1000) % 10;
+    const thirdNumber = Math.floor(counter / 100) % 10;
+    const secondNumber = Math.floor(counter / 10) % 10;
+    const firstNumber = Math.floor(counter / 1) % 10;
+
     return (
         <div className='chrono'>
             <div className='clockIcone'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-clock" viewBox="0 0 16 16">
                     <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
                     <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
                 </svg>
             </div>
-            <div className='eighthNumber'>{props.eighthDigit % 10}</div>
-            <div className='seventhNumber'>{props.seventhDigit % 10}</div>
-            <div className='sixthNumber'>{props.sixthDigit % 10}</div>
-            <div className='fifthNumber'>{props.fifthDigit % 10}</div>
-            <div className='forthNumber'>{props.forthDigit % 10}</div>
-            <div className='thirdNumber'>{props.thirdDigit % 10}</div>
+            <div className='eighthNumber'>{eighthNumber}</div>
+            <div className='seventhNumber'>{seventhNumber}</div>
+            <div className='sixthNumber'>{sixthNumber}</div>
+            <div className='fifthNumber'>{fifthNumber}</div>
+            <div className='forthNumber'>{forthNumber}</div>
+            <div className='thirdNumber'>{thirdNumber}</div>
             <div className='point'>,</div>
-            <div className='secondNumber'>{props.secondDigit % 10}</div>
-            <div className='firstNumber'>{props.firstDigit % 10}</div>
+            <div className='secondNumber'>{secondNumber}</div>
+            <div className='firstNumber'>{firstNumber}</div>
+         
         </div>
-    )
+    );
 };
 
-let counter = 0;
-
-setInterval(function(){
-    const eighthNumber = Math.floor(counter/10000000);
-    const seventhNumber = Math.floor(counter/1000000);
-    const sixthNumber = Math.floor(counter/100000);
-    const fifthNumber = Math.floor(counter/10000);
-    const forthNumber = Math.floor(counter/1000);
-    const thirdNumber = Math.floor(counter/100);
-    const secondNumber = Math.floor(counter/10);
-    const firstNumber = Math.floor(counter/1);
-    counter ++;
-        ReactDOM.render(<SimpleCounter 
-            firstDigit = {firstNumber} 
-            secondDigit = {secondNumber} 
-            thirdDigit = {thirdNumber}
-            forthDigit = {forthNumber}
-            fifthDigit = {fifthNumber}
-            sixthDigit = {sixthNumber}
-            seventhDigit = {seventhNumber}
-            eighthDigit = {eighthNumber} 
-            />, document.querySelector("#app"));
-}, 500);
-
 export default SimpleCounter;
+
+  /*  <div class="d-grid gap-2 col-6 mx-auto">
+                <button onClick={handleStart} class="btn btn-success" type="button">Start</button>
+                <button class="btn btn-warning" type="button">Stop</button>
+            </div> */
